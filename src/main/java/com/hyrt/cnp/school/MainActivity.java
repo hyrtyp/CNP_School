3dabfc4cc8efcd16a202369cc91e3b9e6859e0f3
hyrtyp <13718868826@163.com>
2014-01-22 15:51:10 +0800

    tongyi style

--- src/main/java/com/hyrt/cnp/school/MainActivity.java
+++ src/main/java/com/hyrt/cnp/school/MainActivity.java
@@ -57,8 +57,7 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
+            return null;
         }
     }
 
